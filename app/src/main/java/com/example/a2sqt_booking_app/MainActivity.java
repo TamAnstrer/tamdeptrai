0af3d3cc261fc8b54d05ac3bb789ce7bd44bd188
Tâm Nguyễn <Nguyen Thanh Tam@DESKTOP-K7BLMDI>
2023-04-14 14:55:19 +0700

    first commit

---
+++ app/src/main/java/com/example/a2sqt_booking_app/MainActivity.java
@@ -0,0 +1,18 @@
+package com.example.a2sqt_booking_app;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+//import android.support.v7.app.AppCompatActivity;
+
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.login_hotelbooking); // thay R.layout.activity_main bằng tên layout của bạn
+    }
+
+
+}
